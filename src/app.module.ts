24e81987841746514642cd89310c806b2bf217d8
Neofuturist <e.derevlev@yandex.ru>
2022-12-30 14:34:47 +0300

    bot work

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TelegrafModule } from 'nestjs-telegraf';
+import * as process from 'process';
+import { ConfigModule } from '@nestjs/config';
+ConfigModule.forRoot();
 
 @Module({
-  imports: [],
+  imports: [
+    TelegrafModule.forRoot({
+      token: process.env.TELEGRAM_BOT_TOKEN,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
